57f5cc2904fdc9f73c13835ea2505ff54680f5a4
impersilvatech <impersilvatech@gmail.com>
2026-03-06 22:41:46 +0100

    Create produtos.js

---
+++ js/produtos.js
@@ -0,0 +1,33 @@
+/* =========================================
+   LISTA DE PRODUTOS (produtos.js)
+   ========================================= */
+const produtos = [
+    {
+        id: 1,
+        nome: "Coxinha de Frango",
+        descricao: "Massa crocante com bastante recheio de frango desfiado.",
+        preco: 100,
+        imagem: "img/coxinha.jpg" // Lembre-se de colocar uma imagem com este nome na pasta img!
+    },
+    {
+        id: 2,
+        nome: "Churros com Doce de Leite",
+        descricao: "Porção de churros quentinhos passados no açúcar e canela.",
+        preco: 100,
+        imagem: "img/churros.jpg"
+    },
+    {
+        id: 3,
+        nome: "Hambúrguer Clássico",
+        descricao: "Pão brioche, carne 160g, queijo cheddar e molho especial.",
+        preco: 1500,
+        imagem: "img/hamburguer.jpg"
+    },
+    {
+        id: 4,
+        nome: "Refrigerante em Lata",
+        descricao: "Coca-cola, Fanta ou Sprite (330ml).",
+        preco: 300,
+        imagem: "img/refrigerante.jpg"
+    }
+];
